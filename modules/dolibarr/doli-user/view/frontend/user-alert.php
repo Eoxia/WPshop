05a2344df2f65cf83539506d08574f5c98b846cf
Nicolas <nicolas.domenech34@laposte.net>
2020-07-20 16:02:40 +0200

    Notice warning Pb de connexion

--- modules/dolibarr/doli-user/view/frontend/user-alert.php
+++ modules/dolibarr/doli-user/view/frontend/user-alert.php
@@ -1,23 +1,29 @@
-<div class="wpeo-modal modal-active wps-modal-alert">
-	<div class="modal-container">
+<?php
+/**
+ * Notice d'alerte
+ *
+ * @author    Eoxia <dev@eoxia.com>
+ * @copyright (c) 2011-2020 Eoxia <dev@eoxia.com>.
+ *
+ * @license   AGPLv3 <https://spdx.org/licenses/AGPL-3.0-or-later.html>
+ *
+ * @package   WPshop\Templates
+ *
+ * @since     2.0.0
+ */
 
-		<!-- Entête -->
-		<div class="modal-header">
-			<h2 class="modal-title"><?php esc_html_e( 'There is a problem connecting your account to our ERP', 'wpshop' ); ?></h2>
-			<div class="modal-close"><i class="fas fa-times"></i></div>
-		</div>
+namespace wpshop;
 
-		<!-- Corps -->
-		<div class="modal-content">
-			<p><?php esc_html_e( sprintf( 'Please contact %s at %s to reactivate your account with this message: %s', get_bloginfo( 'name' ), get_bloginfo( 'admin_email' ), $response['status_code'] . ' - ' . $response['status_message'] ), 'wpshop' ); ?></p>
+defined( 'ABSPATH' ) || exit;
 
-			<p><a href="<?php echo wp_logout_url( home_url() ); ?>"><?php esc_html_e('Logout', 'wpshop' ); ?></a></p>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
-		</div>
-
-		<!-- Footer -->
-		<div class="modal-footer">
-			<a class="wpeo-button button-grey button-uppercase modal-close" style="bottom: 45px"><span><?php esc_html_e('Close', 'wpshop' ); ?></span></a>
-		</div>
+<div class="wpeo-notice notice-warning">
+	<div class="notice-content">
+		<div class="notice-title"><?php esc_html_e( 'There is a problem connecting your account to our ERP', 'wpshop' ); ?></div>
+		<div class="notice-subtitle"><?php esc_html_e( sprintf( 'Please contact %s at %s to reactivate your account with this message: %s', get_bloginfo( 'name' ), get_bloginfo( 'admin_email' ), $response['status_code'] . ' - ' . $response['status_message'] ), 'wpshop' ); ?></div>
 	</div>
+	<div class="notice-close"><i class="fas fa-times"></i></div>
 </div>
