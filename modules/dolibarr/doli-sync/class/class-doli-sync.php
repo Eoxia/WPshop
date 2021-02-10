9117e334d5e4dbabe8de28918de35a641b52703f
Théo David <theo.david@evarisk.com>
2021-02-10 10:42:06 +0100

    fix: get invoice date from dolibarr data

--- modules/dolibarr/doli-sync/class/class-doli-sync.php
+++ modules/dolibarr/doli-sync/class/class-doli-sync.php
@@ -279,12 +279,12 @@ class Doli_Sync extends Singleton_Util {
 
 		if ( Settings::g()->debug_mode() ) {
 			//Start
-			echo '<pre>';
-			print_r('L266' );
-			print_r($response);
-			print_r('check dolibarr sync');
-			print_r('4');
-			echo '</pre>';
+//			echo '<pre>';
+//			print_r('L266' );
+//			print_r($response);
+//			print_r('check dolibarr sync');
+//			print_r('4');
+//			echo '</pre>';
 			//exit;
 			//End
 		}
