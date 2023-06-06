4f5eee261718031de2ed5e881313f71b272e618d
Théo David <theo.david@evarisk.com>
2023-06-06 17:44:15 +0200

    #121 [Product] fix: quantity update on product single view

--- modules/products/asset/js/product.frontend.js
+++ modules/products/asset/js/product.frontend.js
@@ -10,41 +10,36 @@ window.eoxiaJS.wpshopFrontend.product = {};
  * La méthode "init" est appelé automatiquement par la lib JS de Eo-Framework.
  *
  * @since 2.0.0
- * @version 2.0.0
+ * @version 2.6.0
  */
 window.eoxiaJS.wpshopFrontend.product.init = function() {
-	jQuery( document ).on( 'click', '.single-wps-product .wps-product .wps-quantity-minus, .single-wps-product .wps-product .wps-quantity-plus', window.eoxiaJS.wpshopFrontend.product.updateQty );
+	jQuery( document ).on( 'click', '.wps-quantity-minus, .wps-quantity-plus', window.eoxiaJS.wpshopFrontend.product.updateQty );
 };
 
 /**
  * Met à jour la quantité d'un produit.
  *
  * @since   2.0.0
- * @version 2.0.0
+ * @version 2.6.0
  *
  * @param {ClickEvent} event [updateQty].
  */
 window.eoxiaJS.wpshopFrontend.product.updateQty = function( event ) {
-	qty = parseInt( jQuery( '.single-wps-product .wps-product .wps-product-quantity .qty' ).text() );
-	var price = parseFloat( jQuery( '.single-wps-product .wps-product .base-price' ).val() );
-	if ( jQuery( this ).hasClass( 'wps-quantity-minus' ) ) {
-		if ( qty > 1 ) {
-			qty--;
-		}
+	let itemQuantity = jQuery('.wps-product-quantity .qty').text();
+	var itemPrice    = parseFloat( jQuery( '.wps-product-content .base-price' ).val() );
+
+	if (jQuery(this).hasClass('wps-quantity-plus')) {
+		itemQuantity++
+	} else {
+		itemQuantity--
 	}
 
-	if ( jQuery( this ).hasClass( 'wps-quantity-plus' ) ) {
-		qty++;
-	}
+	itemQuantity = itemQuantity > 1 ? itemQuantity : 1
 
-	price *= qty;
+	itemPrice *= itemQuantity;
+	itemPrice = new Intl.NumberFormat( 'fr-FR', { style: 'currency', currency: 'EUR' }).format(itemPrice);
 
-	price = new Intl.NumberFormat( 'fr-FR', { style: 'currency', currency: 'EUR' }).format(price);
-
-	jQuery( '.single-wps-product .wps-product' ).find( '.qty' ).text( qty );
-
-	if ( jQuery( '.single-wps-product .wps-product .wps-product-buy' ).length > 0 ) {
-		jQuery( '.single-wps-product .wps-product .wps-product-buy' ).attr( 'data-qty', qty );
-	}
-	jQuery( '.single-wps-product .wps-product .wps-product-price' ).text( price );
+	jQuery('.wps-product-quantity .qty').text(itemQuantity);
+	jQuery('.wps-product-content .wps-product-price').html( itemPrice );
+	jQuery( '.wps-product-buy' ).attr( 'data-qty', itemQuantity );
 };
