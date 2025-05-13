7b55fc31bafe0c60f1304cddf85df0d8ef610e05
evarisk-kilyan <kilyan.evarisk@gmail.com>
2025-05-13 12:17:48 +0200

    #163 [Block] fix: block checkout not working

--- blocks/build/checkout-block/inner-blocks/checkout-button-block/index.asset.php
+++ blocks/build/checkout-block/inner-blocks/checkout-button-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '805dce4623fc0bc5074b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ee498940aef95e0786db');
