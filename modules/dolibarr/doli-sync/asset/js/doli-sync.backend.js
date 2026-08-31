ccea4bd88531d739cff50ea41a353c5c5e97b4d3
Laurent Magnin <laurent1magnin@gmail.com>
2026-09-01 00:19:26 +0200

    fix(sync): refresh product page after clicking sync button

--- modules/dolibarr/doli-sync/asset/js/doli-sync.backend.js
+++ modules/dolibarr/doli-sync/asset/js/doli-sync.backend.js
@@ -124,7 +124,13 @@ window.eoxiaJS.wpshop.doliSync.syncEntrySuccess = function( triggeredElement, re
 	} else if ( jQuery( triggeredElement ).closest( '.table-row' ).length > 0 ) {
 		// If it is sync action per entry.
 		jQuery( triggeredElement ).closest( '.table-row' ).replaceWith( response.data.item_view );
-	} else	{
-		jQuery( triggeredElement ).closest( '.wps-sync-container' ).replaceWith( response.data.sync_view );
+	} else {
+		var syncContainer = jQuery( triggeredElement ).closest( '.wps-sync-container' );
+		if ( syncContainer.length > 0 ) {
+			syncContainer.replaceWith( response.data.sync_view );
+		} else {
+			// Reload the page to reflect the synced changes on the product page
+			window.location.reload();
+		}
 	}
 };
