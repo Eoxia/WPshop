36468bed2d344b97861e1610c64dea5db438ca80
Laurent Magnin <laurent1magnin@gmail.com>
2026-09-01 23:28:11 +0200

    feat(settings): Add a button to reset sync colors to defaults

--- modules/settings/action/class-settings-action.php
+++ modules/settings/action/class-settings-action.php
@@ -71,21 +71,26 @@ class Settings_Action {
 
 		$tab = ! empty( $_POST['tab'] ) ? sanitize_text_field( $_POST['tab'] ) : 'sync';
 
-		$sync_settings = get_option( 'wps_sync_settings', array() );
+		if ( ! empty( $_POST['wps_sync_reset'] ) ) {
+			delete_option( 'wps_sync_settings' );
+			set_transient( 'updated_wpshop_option_' . get_current_user_id(), __( 'Couleurs réinitialisées aux valeurs par défaut.', 'wpshop' ), 30 );
+		} else {
+			$sync_settings = get_option( 'wps_sync_settings', array() );
 
-		if ( isset( $_POST['wps_sync_color_error'] ) ) {
-			$sync_settings['color_error'] = sanitize_hex_color( $_POST['wps_sync_color_error'] );
-		}
-		if ( isset( $_POST['wps_sync_color_orange'] ) ) {
-			$sync_settings['color_orange'] = sanitize_hex_color( $_POST['wps_sync_color_orange'] );
-		}
-		if ( isset( $_POST['wps_sync_color_ok'] ) ) {
-			$sync_settings['color_ok'] = sanitize_hex_color( $_POST['wps_sync_color_ok'] );
-		}
+			if ( isset( $_POST['wps_sync_color_error'] ) ) {
+				$sync_settings['color_error'] = sanitize_hex_color( $_POST['wps_sync_color_error'] );
+			}
+			if ( isset( $_POST['wps_sync_color_orange'] ) ) {
+				$sync_settings['color_orange'] = sanitize_hex_color( $_POST['wps_sync_color_orange'] );
+			}
+			if ( isset( $_POST['wps_sync_color_ok'] ) ) {
+				$sync_settings['color_ok'] = sanitize_hex_color( $_POST['wps_sync_color_ok'] );
+			}
 
-		update_option( 'wps_sync_settings', $sync_settings );
+			update_option( 'wps_sync_settings', $sync_settings );
 
-		set_transient( 'updated_wpshop_option_' . get_current_user_id(), __( 'Your settings have been saved.', 'wpshop' ), 30 );
+			set_transient( 'updated_wpshop_option_' . get_current_user_id(), __( 'Your settings have been saved.', 'wpshop' ), 30 );
+		}
 
 		wp_redirect( admin_url( 'admin.php?page=wps-settings&tab=' . $tab ) );
 		exit;
